e3a5ba2d6d0109d8e7c0fab28c9669837f580bc9
NextKolya <phelixgluy@gmail.com>
2025-11-29 23:01:56 +0600

    Add theme type and theme state in SettingsStore

--- frontend/store/SettingsStore/useSettingsStore.ts
+++ frontend/store/SettingsStore/useSettingsStore.ts
@@ -1,11 +1,21 @@
 import { create } from 'zustand';
 
+import type { Theme } from '@/types/Theme';
+
 interface SettingsStore {
     showNavbar: boolean;
     setShowNavbar: (value: boolean) => void;
+
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
 }
 
 export const useSettingsStore = create<SettingsStore>()((set) => ({
     showNavbar: false,
+
     setShowNavbar: (value) => set({ showNavbar: value }),
+
+    theme: 'ChatGPT',
+
+    setTheme: (theme) => set({ theme }),
 }));
---
+++ frontend/types/Theme.ts
@@ -0,0 +1,1 @@
+export type Theme = 'None Dark' | 'None Light' | 'ChatGPT';
