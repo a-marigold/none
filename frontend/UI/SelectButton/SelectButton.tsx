6b450d197a19c0addb3a40aacd8ff98235c8e7da
NextKolya <phelixgluy@gmail.com>
2025-11-29 17:22:38 +0600

    Add SelectButton UI component

---
+++ frontend/UI/SelectButton/SelectButton.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes } from 'react';
+
+import buttonStyles from './SelectButton.module.scss';
+
+interface SelectButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    title: string;
+}
+
+export default function SelectButton({
+    title,
+
+    className,
+    ...attributes
+}: SelectButtonProps) {
+    return (
+        <button
+            {...attributes}
+            className={`${buttonStyles['select-button']} ${className || ''}`}
+        >
+            <span className={buttonStyles['title']}>{title}</span>
+
+            <svg width={16} height={16} color='var(--font-color)'>
+                <use href='#accordion-icon' />
+            </svg>
+        </button>
+    );
+}
