42ef6c6ad01dbb93d7ffa5da4a930771707a291e
NextKolya <phelixgluy@gmail.com>
2025-12-06 14:22:49 +0600

    Add chatStackLength state in SettingsStore

--- frontend/store/SettingsStore/useSettingsStore.ts
+++ frontend/store/SettingsStore/useSettingsStore.ts
@@ -3,12 +3,19 @@ import { persist } from 'zustand/middleware';
 
 import type { ThemeName } from '@/types/Theme';
 
+// TODO: remove the settings persistance in DB
+
 interface SettingsStore {
     showNavbar: boolean;
     setShowNavbar: (value: boolean) => void;
 
     currentTheme: ThemeName;
+
     setCurrentTheme: (theme: ThemeName) => void;
+
+    chatStackLength: number;
+
+    setChatStackLength: (value: number) => void;
 }
 
 export const useSettingsStore = create<SettingsStore>()(
@@ -21,10 +28,16 @@ export const useSettingsStore = create<SettingsStore>()(
             currentTheme: 'ChatGPT',
 
             setCurrentTheme: (theme) => set({ currentTheme: theme }),
+
+            chatStackLength: 3,
+            setChatStackLength: (value) => set({ chatStackLength: value }),
         }),
         {
             name: 'settings-store',
-            partialize: (state) => ({ currentTheme: state.currentTheme }),
+            partialize: (state) => ({
+                currentTheme: state.currentTheme,
+                chatStackLength: state.chatStackLength,
+            }),
         }
     )
 );
